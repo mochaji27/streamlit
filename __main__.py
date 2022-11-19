b9c4e934ad9648fc3e36c144c010144049d25c7c
mochaji27 <muhamadaji1020@gmail.com>
2022-11-20 01:47:42 +0700

    Upload

--- __main__.py
+++ __main__.py
@@ -2,40 +2,26 @@ import streamlit as st
 import pandas as pd
 import pandas_profiling 
 from streamlit_pandas_profiling import st_profile_report
-
-def check_file(file_upload):
-    if file_upload is None:
-        return
-    if ".csv" in file_upload.name:
-        return pd.read_csv(file_upload, sep=",")
-    if ".xlsx" in file_upload.name:
-        return pd.read_excel(file_upload)
-    return
+import utils
     
 
 def main_page():
     st.title("Generate Machine Learning")
-    st.write("1. Upload your data")
+    st.write("Upload your data")
     file_upload = st.file_uploader("Choose a XLSX file", type=["xlsx", "csv"])
-    df = check_file(file_upload)
-    if df is not None:
-        pr = df.profile_report()
-        st.title("Data Profiling")
-        st_profile_report(pr)
+    if file_upload is not None:
+        df = utils.check_file(file_upload)
+        tab_view, tab_profile, tab_predict = st.tabs(["View Data", "Data Profiling", "Predict"])
+        with tab_view:
+            st.dataframe(df)
+        with tab_profile:
+            pr = df.profile_report()
+            st_profile_report(pr)
+
+
     
 
 
 
 if __name__=="__main__":
-    with st.sidebar:
-        st.title("""
-            Option
-        """)
-        add_radio = st.radio(
-            "Choose a Shipping method",
-            ("Standard", "Express")
-        )
-    if add_radio == "Standard":
-        main_page()
-    else:
-        st.title("Test")
+    main_page()
---
+++ utils.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+
+def check_file(file_upload):
+    if file_upload is None:
+        return
+    if ".csv" in file_upload.name:
+        return pd.read_csv(file_upload, sep=",")
+    if ".xlsx" in file_upload.name:
+        return pd.read_excel(file_upload)
+    return
