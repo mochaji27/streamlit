adaf900f05e1bb5e1504b5b824f4b706e14058ff
Mochamad Aji Pangestu <64255690+mochaji27@users.noreply.github.com>
2024-11-28 15:30:18 +0700

    Update classification.py

--- page/classification.py
+++ page/classification.py
@@ -182,7 +182,7 @@ def prediction(model_upload, file_upload):
 def model(df, target, list_col_exc):
     if st.session_state["preprocess_option"] == "Default":
         print("------------------- ini adalah default ------------------------")
-        pycc.setup(df, target = target, train_size=0.75, silent = True, use_gpu = True, ignore_features=(list_col_exc), session_id = 123)
+        pycc.setup(df, target = target, train_size=0.75, use_gpu = True, ignore_features=(list_col_exc), session_id = 123)
     else:
         print("------------------- ini adalah advanced ------------------------")
         pycc.setup(df, target = target, train_size=0.75, 
