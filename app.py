1de2acc550cac0de2cf5f363ec232dc7aad31134
Mochamad Aji Pangestu <64255690+mochaji27@users.noreply.github.com>
2024-12-29 20:05:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ import streamlit as st
 import page.classification as cla
 import page.regression as reg
 import page.clustering as clu
-    
+import page.gelo as gelo    
 
 
 if __name__=="__main__":
     type = st.sidebar.radio(
         "Choose Machine Learning type",
-        ("Classification", "Regression", "Clustering")
+        ("GeLo", "Classification", "Regression", "Clustering")
         )
     if type == "Classification":
         cla.main_page()
 
     if type == "Regression":
-        reg.main_page()
+       reg.main_page()
 
     if type == "Clustering":
-        clu.main_page()
+       clu.main_page()
+
+    if type == "GeLo":
+       gelo.main_page()
